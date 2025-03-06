1de944756f62b1bbd92ea1654e6ad251fb15a5d1
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-03-06 18:42:13 +0700

    Add files via upload
    Add class FullTimeEmployee

---
+++ FullTimeEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeManagement
+{
+
+    public class FullTimeEmployee : Employee
+    {
+        public FullTimeEmployee(string name, int paymentPerHour)
+            : base(name, paymentPerHour)
+        {
+        }
+        public override int calculateSalary()
+        {
+            const int WORKING_HOURS_PER_DAY = 8;
+            const int WORKING_DAYS_PER_MONTH = 20;
+            return paymentPerHour * WORKING_HOURS_PER_DAY * WORKING_DAYS_PER_MONTH;
+        }
+    }
+}
