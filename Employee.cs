7856604ccf77f1e2cbcf939a5dd930a42f068626
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-03-06 18:41:36 +0700

    Add files via upload
    Add class Employee

---
+++ Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeeManagement
+{
+    public abstract class Employee : IEmployee
+    {
+        protected string name;
+        protected int paymentPerHour;
+
+        public Employee(string name, int paymentPerHour)
+        {
+            this.name = name;
+            this.paymentPerHour = paymentPerHour;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public abstract int calculateSalary();
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setPaymentPerHour(int paymentPerHour)
+        {
+            this.paymentPerHour = paymentPerHour;
+        }
+
+        public int getPaymentPerHour()
+        {
+            return paymentPerHour;
+        }
+    }
+}
