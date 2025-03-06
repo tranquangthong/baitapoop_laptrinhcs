a17abe2c0206f686d8df25f4de0c8c1a0140e203
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-03-06 18:42:34 +0700

    Add files via upload
    Add class PartTimeEmployee

---
+++ PartTimeEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeManagement
+{
+    public class PartTimeEmployee : Employee
+    {
+        private int workingHours;
+
+        public PartTimeEmployee(string name, int paymentPerHour, int workingHours)
+            : base(name, paymentPerHour)
+        {
+            this.workingHours = workingHours;
+        }
+
+        public override int calculateSalary()
+        {
+            return paymentPerHour * workingHours;
+        }
+    }
+}
